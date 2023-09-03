19f50537ff347b256e891c9a103aa2c1982b5afb
oxygen000 <abdelhamedreda@outlook.com>
2023-09-03 15:57:52 +0300

    abdelhamed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { PromoComponent } from './promo/promo.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    HomeComponent,
+    ProductComponent,
+    PromoComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
